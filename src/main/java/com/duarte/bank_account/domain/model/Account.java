1e83e7ab4b201d548e6687764d74e01c4aa27306
Samuel Duarte <sduartegit@gmail.com>
2025-04-11 12:24:53 -0300

    first commit

---
+++ src/main/java/com/duarte/bank_account/domain/model/Account.java
@@ -0,0 +1,18 @@
+package com.duarte.bank_account.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    
+    private String fullName;
+
+    private String email;
+
+}
+
